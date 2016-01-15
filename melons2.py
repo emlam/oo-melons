9d83b027a0f3a6c26fef252568d80ba969a938fa
Hackbright Student <info@hackbrightacademy.com>
2016-01-15 13:00:12 -0800

    created Christmas melon fee

--- melons2.py
+++ melons2.py
@@ -3,10 +3,10 @@
 class AbstractMelonOrder(object):
     """ You fill in the rest """
     
-    def __init__(self, species, qty, order_type, tax, country_code = None):
+    def __init__(self, species, qty, tax, order_type = "domestic", country_code = None):
         """Initializing a melon order"""
 
-        self.species = species
+        self.species = species 
         self.qty = qty
         self.shipped = False
         self.base_price = 5
@@ -22,27 +22,35 @@ class AbstractMelonOrder(object):
 
     def get_total(self):
         """Calculate price."""
-
-        base_price = 5
-        total = (1 + self.tax) * self.qty * base_price
-        return total
-
-
+        if self.species == "Christmas":
+            base_price = 5 * 1.5
+            total = (1 + self.tax) * self.qty * base_price
+            return total
+        else:
+            base_price = 5
+            total = (1 + self.tax) * self.qty * base_price
+            return total
+        
 
 class DomesticMelonOrder(AbstractMelonOrder):
     """ You fill in the rest """
 
     def __init__(self, species, qty):
-        super(DomesticMelonOrder,self).__init__(species,qty, "domestic", .08)
+        super(DomesticMelonOrder,self).__init__(species, qty, .08)
 
 
 class InternationalMelonOrder(AbstractMelonOrder):
-    """ You fill in the rest """   
+    """ You fill in the rest """    
 
         
     def __init__(self, species, qty, country_code):
-        super(InternationalMelonOrder,self).__init__(species, qty, "international", .17, country_code)
-        
+        super(InternationalMelonOrder,self).__init__(species, qty, .18, "international", country_code)
+
+    # def get_total(self):
+    #     super(InternationalMelonOrder,self).get_total()
+    #     if self.qty <10:
+    #         return total + 3
+
 
     def get_country_code(self):
         """Return the country code."""
